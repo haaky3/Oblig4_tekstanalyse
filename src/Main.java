b978710511c67b59dbb94a86918a710cb2910132
Håkon Johansen <hakon.johansen@hiof.no>
2017-04-18 14:59:35 +0200

    Det gikk

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String fileName = "churchill.txt";
+        ClassLoader classLoader = new Main().getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+         
+        //File is found
+        System.out.println("File Found : " + file.exists());
+         
+        //Read File Content
+        String content;
+		try {
+			content = new String(Files.readAllBytes(file.toPath()));
+			System.out.println(content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
 	}
 
 }
