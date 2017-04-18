99ec5ca481787c5f90df2d007fb492a3703fea25
Håkon Johansen <hakon.johansen@hiof.no>
2017-04-18 17:01:56 +0200

    Comments added

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,46 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//Vi valgte å bruke resource-filer som tekst i dette prosjektet.
+		//Selvfølgelig bruker vi Winston Churchill sin tale som fil.
+		//Kunne ha implementert valg om ulike tekstfiler, men vi er nær innleveringsfrist, så vi står over ;)
 		
+		//Henter tekstdokumentet i resource-mappen
 		String fileName = "churchill.txt";
         ClassLoader classLoader = new Main().getClass().getClassLoader();
         File file = new File(classLoader.getResource(fileName).getFile());
          
-
-        System.out.println("File Found : " + file.exists());
-         
-        //Read File Content
+        
         String[] words = null;
 		try {
+			//Leser innholder til filen
 			String content = new String(Files.readAllBytes(file.toPath())).toUpperCase();
+			
+			//Splitter opp teksten 
+			//Litt dårlig regex, siden den splitter opp store tall og fnutter
+			//Eks. "100,000" -> ["100", "000"] og "Hitler's" -> ["Hitler", "s"]
 			words = content.split("\\W+");
 
 				
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			// Greier ikke lese filen. Feil med filen
 			e.printStackTrace();
-			System.exit(0);
+			System.exit(0); //Stopper programmet hvis filen ikke eksisterer
 		}
 		
+		//Oppretter det binære treet
 		Tree wordTree = new Tree();
 		
+		//Legger inn alle ord i treet
 		for(String w: words){
 			wordTree.insert(w);
 		}
 		
+		//Printer ut treet alfabetisk (inorder).
 		wordTree.printInOrder(wordTree.root);
 		
 	}
--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,9 @@
+//Hovedtre
 public class Tree {
 	
+	//Noder til treet
 	public class Node {
+		
 		public String word;
 		public int count;
 		public Node left, right;
@@ -10,65 +13,73 @@ public class Tree {
 			this.left = this.right = null;
 			this.count = 1;
 		}
-		
-		@Override
-		public String toString() {
-			return this.word + "\t\t\t\t" + this.count;
-		}
+			
 	}
 	
 	Node root;
 	
+	//Når treet blir opprettet blir en tom node lagt inn
 	public Tree()
     {
 		root = null;
     }
 	
+	//Sjekk om tre er tomt
 	public boolean isEmpty()
     {
 		return (root == null);
     }
 	
-	public void insert(String value)
+	//Legger til ny node eller høyner count på noder som finnes
+	public void insert(String w)
     {
 
-		// Create new root if tree is empty
+		// Hvis treet er tomt blir første node lagt inn
 		if (isEmpty())
 		{
-		    root = new Node(value);
+		    root = new Node(w);
 		    return;
 		}
 
 		Node current = root;
 		boolean finished = false;
 		
-		// Insert new node as a leaf in the tree
+		// Leter etter node i treet. 
 		while (!finished)
 		{
-			int compare = current.word.compareTo(value);
+			//Sjekker noden og ordet
+			int compare = current.word.compareTo(w);
 			
+			//HVis vi finner en node i treet som allerede har samme verdi,
+			//så skal ingen ny node blir lagt inn, men count til noden blir større.
 			if(compare == 0){
 				current.count++;
 				finished = true;
 			}
+			//Ordet er alfabetisk før noden vi sjekker
 			else if (compare > 0)
 		    {
+				//Hvis nodens venstre node er tom, så lages en ny node med ordet
 				if (current.left == null)
 				{
-				    current.left = new Node(value);
+				    current.left = new Node(w);
 				    finished = true;
 				}
+				//Hvis ikke nodens venstre node er tom, går vi til den og sjekker igjen.
 				else {
 				    current = current.left;
 				}
 			}
+			//Ordet er alfabetisk etter noden vi sjekker
 		    else if (compare < 0)
 		    {
+		    	//Hvis nodens høyre node er tom, så lages en ny node med ordet
 				if (current.right == null)
 				{
-				    current.right = new Node(value);
+				    current.right = new Node(w);
 				    finished = true;
 				}
+				//Hvis ikke nodens høyre node er tom, går vi til den og sjekker igjen.
 				else {
 				    current = current.right;
 				}
@@ -76,9 +87,11 @@ public class Tree {
 		}
 	}
 	
+	//Printer ut fra satt node hele treet inorder.
 	public void printInOrder(Node n){
 		if(n != null){
 			printInOrder(n.left);
+			//Formatering i terminal for utskrift
 			System.out.printf("%-20s%s%n",n.word,n.count);
 			printInOrder(n.right);
 		}
